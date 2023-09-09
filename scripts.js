ba0772c49cd45b17523bf0ae9e711046f3dc71d0
David Kabanda <116374315+MBChB-Dev@users.noreply.github.com>
2023-09-09 20:25:13 +0200

    Add grid size button

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+// Create grid
+
 const grid = document.querySelector(".grid-container");
 
 const fragment = document.createDocumentFragment();
@@ -5,9 +7,26 @@ const fragment = document.createDocumentFragment();
 for (let i = 0; i < 256; i++) {
     const square = document.createElement("div");
     square.style.cssText = "border: 4px solid black; flex: 1 1 6.25%; background-color: white;";
+    square.classList.add("grid-squares");
     fragment.appendChild(square);
 }
 
 grid.appendChild(fragment);
+
+// Add event listener
+
+let gridSquares = document.querySelectorAll(".grid-squares");
+
+function hoverEffect() {
+    this.style.cssText = "border: 4px solid black; flex: 1 1 6.25%; background-color: black;";
+}
+
+gridSquares.forEach(function(square) {
+    square.addEventListener('mouseenter', hoverEffect);
+    square.addEventListener('touchstart', hoverEffect);
+})
+
+// Add grid size button
+
 
 
