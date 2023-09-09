81cf78c2e7e858fc60b2d515318a0641807bc4e0
David Kabanda <116374315+MBChB-Dev@users.noreply.github.com>
2023-09-09 21:03:30 +0200

    Create function to play game

--- scripts.js
+++ scripts.js
@@ -1,32 +1,52 @@
-// Create grid
+function startGame(gridSizeInput) {
+    
+    // Create grid
 
-const grid = document.querySelector(".grid-container");
+    const grid = document.querySelector(".grid-container");
 
-const fragment = document.createDocumentFragment();
+    const fragment = document.createDocumentFragment();
 
-for (let i = 0; i < 256; i++) {
-    const square = document.createElement("div");
-    square.style.cssText = "border: 4px solid black; flex: 1 1 6.25%; background-color: white;";
-    square.classList.add("grid-squares");
-    fragment.appendChild(square);
+    const totalSquares = gridSizeInput * gridSizeInput;
+
+    const squareDimensions = ((1 / gridSizeInput) * 100);
+
+    for (let i = 0; i < totalSquares; i++) {
+        const square = document.createElement("div");
+        square.style.cssText = `border: 4px solid black; flex: 1 1 ${squareDimensions}%; background-color: white;`;
+        square.classList.add("grid-squares");
+        fragment.appendChild(square);
+    }
+
+    grid.appendChild(fragment);
+
+    // Add event listener for hover effect
+
+    let gridSquares = document.querySelectorAll(".grid-squares");
+
+    function hoverEffect() {
+        this.style.cssText = "border: 4px solid black; flex: 1 1 6.25%; background-color: black;";
+    }
+
+    gridSquares.forEach(function(square) {
+        square.addEventListener('mouseenter', hoverEffect);
+        square.addEventListener('touchstart', hoverEffect);
+    })
 }
 
-grid.appendChild(fragment);
 
-// Add event listener
-
-let gridSquares = document.querySelectorAll(".grid-squares");
-
-function hoverEffect() {
-    this.style.cssText = "border: 4px solid black; flex: 1 1 6.25%; background-color: black;";
-}
-
-gridSquares.forEach(function(square) {
-    square.addEventListener('mouseenter', hoverEffect);
-    square.addEventListener('touchstart', hoverEffect);
-})
 
 // Add grid size button
+
+const sizeButton = document.querySelector(".sizeButton");
+
+sizeButton.addEventListener("click", () => {
+    let gridSize = window.prompt("Enter the number of squares per side for the new grid:");
+    startGame(gridSize);
+})
+
+//Start the game with default size
+
+startGame(16);
 
 
 
